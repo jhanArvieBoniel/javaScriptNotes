d72a7236906c81ec07bbfb85ba9bcce3789d47f1
gaspar619 <jhanarvie@gmail.com>
2023-02-15 03:32:09 +0100

    command in git bash

---
+++ command.cmd.js
@@ -0,0 +1,25 @@
+console.log("----all the command in terminal-----")
+console.log("cd  -change directory EX. ~/Desktop (linux) from directory to desktop directory")
+console.log("cd.. -change directory move out from current to parent directory")
+console.log("touch -will make a new empty file EX. touch example.txt or example\ space.js")
+console.log("mkdir + (title) -making new folders EX. mkdir word-docs")
+console.log("history -view history of recently typed command")
+console.log("SAMPLE")
+console.log(`
+1. Change ot Desktop directory by typing cd~/Desktop
+2. Create folder by typing mkdir 
+3. Change to folder directory by typing cd myjsproject
+4. Create file by typing example.js
+5. Open file example.js by VScode 
+`)
+console.log("if file has space. type on command line: this is a sample ===cmd==>  cd this\ is\ a\ sample")
+console.log("man -will display a manual")
+console.log("man ls -will show a detailed command for ls")
+console.log("ls -will show the current working directory")
+console.log("ls -l -list the file out in list order")
+console.log("ls -a -will list all file directories including hidden ones")
+console.log("pwd -print working deirectory shows the full path of the current working directory")
+console.log("cp -copies files/folder from one destination to another")
+console.log("mv -move file/directory from one to another: mc sample.txt sampleFolder")
+console.log("after typing ls -l, know the difference between link file, directory, and standard file")
+console.log("link file is represented by l, directory starts with d, and - for standard file")
